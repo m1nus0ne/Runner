65e03c5ff81fde46e2f1977acc91483e70c809e5
Калентьев Илья <m1nus0ne@mail.ru>
2026-04-09 17:55:07 +0500

    cleanup

---
+++ src/Modules/Submissions/Runner.Submissions.Module/Infrastructure/Database/Migrations/20260409001724_IncreaseErrorMessageMaxLength.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Runner.Submissions.Module.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseErrorMessageMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ErrorMessage",
+                schema: "submissions",
+                table: "test_group_results",
+                type: "character varying(8000)",
+                maxLength: 8000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ErrorMessage",
+                schema: "submissions",
+                table: "test_group_results",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(8000)",
+                oldMaxLength: 8000,
+                oldNullable: true);
+        }
+    }
+}
